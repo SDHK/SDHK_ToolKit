cdaa5e3570f377f23ebcbf93d6073459e6355290
wujingsi <630168819@qq.com>
2022-06-09 11:54:54 +0800

    修改001

--- _Global/Other/GitTest001.cs
+++ _Global/Other/GitTest001.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assets.SDHK_Tool._Global.Other
+namespace Assets.SDHK_Tool._Global.Other.Git
 {
     public class GitTest001
     {
+        public string text1 = "Hello World";
+
     }
 }
