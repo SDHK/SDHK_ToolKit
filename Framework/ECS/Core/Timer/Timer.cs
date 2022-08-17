7e8b926f41dec094ce6d34518732c4e91704646d
wujingsi <630168819@qq.com>
2022-08-17 20:02:49 +0800

    思路补充

--- Framework/ECS/Core/Timer/Timer.cs
+++ Framework/ECS/Core/Timer/Timer.cs
@@ -7,8 +7,8 @@ using UnityEngine;
 
 namespace SDHK
 {
-    //需要全局暂停的考虑，所以只能用时间累计，不能用现实时间和时间差进行计算
-    //时域应该与计时器无关，计时器的作用是回调
+    //考虑全局暂停，所以只能用时间累加，不能用系统时间相减，进行时间差进行计算
+    //时域+计时器，时域是float, 计时器的作用是回调事件
 
     public class TimerManager : Entity
     {
