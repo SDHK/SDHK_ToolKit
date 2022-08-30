5aa56cf200eb49a8ef325cdb63ec4b97dc198625
wujingsi <630168819@qq.com>
2022-08-30 09:43:19 +0800

    Revert "增加GUI组件"
    This reverts commit c6ce6fbc43865ceb1801ea64dc2dac130793d968.

--- WorldTreeFramework/UnityEnvironment/WorldTreeConsole/WorldTreeConsoleManager.cs
+++ WorldTreeFramework/UnityEnvironment/WorldTreeConsole/WorldTreeConsoleManager.cs
@@ -19,38 +19,6 @@ namespace WorldTree
     {
         public override void OnAdd(ConsoleTreeView self)
         {
-
-            self.beginVertical1 = self.AddChildren<GUIBeginVertical>();
-            self.beginVertical1.Texture = self.GetColorTexture(new Color(0.1f, 0.1f, 0.1f, 1));
-            self.beginVertical1.Padding = new RectOffset(5, 5, 5, 5);
-
-            self.beginVertical2 = self.AddChildren<GUIBeginVertical>();
-            self.beginVertical2.Texture = self.GetColorTexture(new Color(0.2f, 0.2f, 0.2f, 1));
-            self.beginVertical2.Padding = new RectOffset(5, 5, 5, 5);
-
-
-            self.beginHorizontal1 = self.AddChildren<GUIBeginHorizontal>();
-            self.beginHorizontal1.Texture = self.GetColorTexture(new Color(0.1f, 0.1f, 0.1f, 1));
-            self.beginHorizontal1.Padding = new RectOffset(5, 5, 5, 5);
-
-            self.beginHorizontal2 = self.AddChildren<GUIBeginHorizontal>();
-            self.beginHorizontal2.Texture = self.GetColorTexture(new Color(0.2f, 0.2f, 0.2f, 1));
-            self.beginHorizontal2.Padding = new RectOffset(5, 5, 5, 5);
-
-
-            self.lineHorizontal = self.AddChildren<GUIBox>();
-            self.lineHorizontal.Texture = self.GetColorTexture(new Color(0.1f, 0.1f, 0.1f, 1));
-            self.lineHorizontal.Height = 2;
-
-            self.lineVertical = self.AddChildren<GUIBox>();
-            self.lineVertical.Texture = self.GetColorTexture(new Color(0.1f, 0.1f, 0.1f, 1));
-            self.lineVertical.Width = 2;
-
-            self.title = self.AddChildren<GUIBox>();
-            self.title.text = "世界树可视化";
-            self.title.FontAnchor = TextAnchor.MiddleLeft;
-
-
             self.systems = self.RootGetSystemGroup<IConsoleTreeViewItemSystem>();
 
             self.componentShowSwitchs = self.Root.ObjectPoolManager.Get<UnitDictionary<long, bool>>();
@@ -65,8 +33,35 @@ namespace WorldTree
             }
             self.currentNode = self.Root;
 
+            self.black3 = new GUIStyle("Label");
+            self.black3.normal.background = new Texture2D(1, 1, Texture2D.grayTexture.format, true);
+            self.black3.normal.background.SetPixel(0, 0, new Color(0.3f, 0.3f, 0.3f));
+            self.black3.normal.background.Apply();
+
+            self.black2 = new GUIStyle("Label");
+            self.black2.normal.background = new Texture2D(1, 1, Texture2D.grayTexture.format, true);
+            self.black2.normal.background.SetPixel(0, 0, new Color(0.2f, 0.2f, 0.2f));
+            self.black2.normal.background.Apply();
+
+            self.black1 = new GUIStyle("Label");
+            self.black1.normal.background = new Texture2D(1, 1, Texture2D.grayTexture.format, true);
+            self.black1.normal.background.SetPixel(0, 0, new Color(0.1f, 0.1f, 0.1f));
+            self.black1.normal.background.Apply();
 
 
+            self.blue = new GUIStyle("Label");
+            self.blue.normal.background = new Texture2D(1, 1, Texture2D.grayTexture.format, true);
+            self.blue.normal.background.SetPixel(0, 0, new Color(0.2f, 0.3f, 0.5f));
+            self.blue.normal.background.Apply();
+
+            self.Transparent = new GUIStyle("Label");
+            self.Transparent.normal.background = new Texture2D(1, 1, Texture2D.grayTexture.format, true);
+            self.Transparent.normal.background.SetPixel(0, 0, new Color(0, 0, 0, 0));
+            self.Transparent.normal.background.Apply();
+
+
+            self.BtnStyle = new GUIStyle("Label");
+            self.BtnStyle.alignment = TextAnchor.MiddleLeft;
         }
     }
 
@@ -101,139 +96,95 @@ namespace WorldTree
     {
         public override void OnGUI(ConsoleTreeView self, float deltaTime)
         {
-            Rect rect = new Rect(self.rect.x, self.rect.y, self.rect.width * GUIDefault.size, self.rect.height * GUIDefault.size);
-            rect = GUI.Window(self.GetHashCode(), rect, self.GUIWindowMax, default(string));
-            self.rect.x = rect.x;
-            self.rect.y = rect.y;
+            self.rect = GUI.Window(self.GetHashCode(), self.rect, self.GUIWindowMax, default(string), GUIDefault.StyleBlack2);
         }
+
     }
     public class ConsoleTreeView : Entity
     {
         public UnitDictionary<long, bool> componentShowSwitchs;
         public UnitDictionary<long, bool> childrenShowSwitchs;
-        public Rect rect = new Rect(0, 0, 800, 600);
+        public Rect rect = new Rect(0, 0, 1000, 1000);
 
         public SystemGroup systems;
         public Entity currentNode;
         public Entity selectNode;
         private Vector2 scrollLogView = Vector2.zero;
 
-        //public GUIStyle Box1;
-        //public GUIStyle Box2;
-        public GUIBox title;
-        public GUIBox lineHorizontal;
-        public GUIBox lineVertical;
-        public GUIBeginHorizontal beginHorizontal1;
-        public GUIBeginHorizontal beginHorizontal2;
-        public GUIBeginVertical beginVertical1;
-        public GUIBeginVertical beginVertical2;
+
+        public string box = "Button";
+        public string box2 = "Box";
+        public GUIStyle BtnStyle;
+
+        public GUIStyle black3;
+        public GUIStyle black2;
+        public GUIStyle black1;
+        public GUIStyle blue;
+        public GUIStyle Transparent;
+
+        private Color bak;
 
         public void GUIWindowMax(int windowId)
         {
-            beginVertical1.Draw();
 
-            beginHorizontal2.Draw();
+            bak = GUI.color;
 
-            title.Draw();
+            //GUI.color = Color.yellow;
 
-            GUILayout.Space(20);
+            GUILayout.BeginHorizontal(GUIDefault.StyleBlack2, GUIDefault.optionHeight25);
 
-            lineVertical.Draw();
-
-            GUIDefault.LeftButton(() => { GUIDefault.size--; });
-            GUILayout.Label(GUIDefault.size.ToString(), GUIDefault.StyleBlack1, GUIDefault.OptionWidth60);
-            GUIDefault.RightButton(() => { GUIDefault.size++; });
-
-            lineVertical.Draw();
-
-            GUILayout.Space(20);
-
-            lineVertical.Draw();
-
-            GUIDefault.LeftButton(() => { rect.width = rect.width - 100; });
-            GUILayout.Label(rect.width.ToString(), GUIDefault.StyleBlack1, GUIDefault.OptionWidth60);
-            GUIDefault.RightButton(() => { rect.width = rect.width + 100; });
-            lineVertical.Draw();
-
-            GUILayout.Space(20);
-
-            lineVertical.Draw();
-            GUIDefault.LeftButton(() => { rect.height = rect.height - 100; });
-            GUILayout.Label(rect.height.ToString(), GUIDefault.StyleBlack1, GUIDefault.OptionWidth60);
-            GUIDefault.RightButton(() => { rect.height = rect.height + 100; });
-            lineVertical.Draw();
-
+            GUILayout.Label("世界树可视化", GUIDefault.StyleTransparent);
             GUILayout.FlexibleSpace();
 
-            GUIDefault.CloseButton(() => { this.RemoveSelf(); });
+            GUIDefault.CloseButton(() => { });
 
             GUILayout.EndHorizontal();
 
 
-
-            lineHorizontal.Draw();
-
-            GUIWindowContent();
-
-            GUILayout.EndVertical();
-            GUI.DragWindow();
-        }
+            GUIDefault.LineHorizontal();
 
 
 
-
-
-        public void GUIWindowContent()
-        {
-
-            beginHorizontal1.Draw();
+            GUILayout.BeginHorizontal(GUIDefault.StyleBlack2);
 
             PathNodeView(currentNode);
 
             GUILayout.EndHorizontal();
 
-            lineHorizontal.Draw();
-            //GUIDefault.LineHorizontal();
+
+            GUIDefault.LineHorizontal();
+
+            //GUI.color = bak;
 
 
-            beginHorizontal2.Draw();
 
-            GUILayout.FlexibleSpace();
-            GUILayout.Label("聚焦", GUIDefault.StyleBlack3);
-            GUILayout.Label("全展开", GUIDefault.StyleBlack3);
-            GUILayout.Label("全折叠", GUIDefault.StyleBlack3);
-            GUILayout.Label("结构/信息", GUIDefault.StyleBlack3);
-
-            GUILayout.EndHorizontal();
-
-
-            lineHorizontal.Draw();
-            //GUIDefault.LineHorizontal();
-
-
-            beginVertical1.Draw();
+            GUILayout.BeginVertical();
 
             scrollLogView = GUILayout.BeginScrollView(scrollLogView, GUIDefault.StyleBlack2);
-
             //TraversalRecursive(currentNode);
             ForeachShow(currentNode);
-
             GUILayout.EndScrollView();
 
             GUILayout.EndVertical();
-        }
+            GUI.DragWindow();
 
+            //子物体绘制
+        }
 
         public void TraversalRecursive(Entity node)
         {
             GUILayout.BeginVertical();
 
-            ShowItem(node, this);
+            ShowItem(node);
 
+            //GUILayout.BeginHorizontal();
 
+            //GUILayout.Space(100);
             ForeachShow(node);
 
+            //GUILayout.EndHorizontal();
 
+            //GUIDefault.LineHorizontal();
 
             GUILayout.EndVertical();
         }
@@ -251,76 +202,24 @@ namespace WorldTree
             }
         }
 
-        public void ShowItem(Entity self, ConsoleTreeView console)
+
+        public void ShowItem(Entity node)
         {
-
-            bool componentSwitch = console.componentShowSwitchs[self.id];
-            bool childrenSwitch = console.childrenShowSwitchs[self.id];
-
-            //GUILayout.BeginHorizontal("Box", GUILayout.Width(600));
-            GUILayout.BeginHorizontal(console.selectNode == self ? GUIDefault.StyleBlue : GUIDefault.StyleBlack3, GUILayout.Width(600));
-            //EditorGUILayout
-
-            if (self.components != null || self.children != null)
+            if (systems.TryGetValue(node.Type, out List<ISystem> systemList))
             {
-                GUIDefault.FoldoutButton(componentSwitch || childrenSwitch, (bit) =>
+                foreach (IConsoleTreeViewItemSystem system in systemList)
                 {
-                    componentSwitch = !bit;
-                    childrenSwitch = componentSwitch;
-                });
-            }
-            else
-            {
-                GUILayout.Space(25);
-            }
-
-
-            GUILayout.BeginHorizontal();
-
-            GUIDefault.Button(self.Type.Name, () =>
-            {
-                if (console.selectNode == self)
-                {
-                    console.currentNode = self;
+                    system.Draw(node, this);
                 }
-                console.selectNode = self;
-            }, GUILayout.Width(300));
-
-
-            if (self.components != null)
+            }
+            else if (systems.TryGetValue(typeof(Entity), out systemList))
             {
-                if (GUILayout.Button(componentSwitch ? "▼" : "▶", GUIDefault.StyleTransparent, GUILayout.Width(20)))
+                foreach (IConsoleTreeViewItemSystem system in systemList)
                 {
-                    componentSwitch = !componentSwitch;
+                    system.Draw(node, this);
                 }
-                GUILayout.Label(self.components.Count.ToString(), GUILayout.Width(80));
             }
-            else
-            {
-                componentSwitch = false;
-            }
-
-
-            if (self.children != null)
-            {
-                if (GUILayout.Button(childrenSwitch ? "▼" : "▶", GUIDefault.StyleTransparent, GUILayout.Width(20)))
-                {
-                    childrenSwitch = !childrenSwitch;
-                }
-                GUILayout.Label(self.children.Count.ToString(), GUILayout.Width(80));
-            }
-            else
-            {
-                childrenSwitch = false;
-            }
-
-            console.componentShowSwitchs[self.id] = componentSwitch;
-            console.childrenShowSwitchs[self.id] = childrenSwitch;
-
-            GUILayout.EndHorizontal();
-            GUILayout.EndHorizontal();
         }
-
         public void ForeachShow(Entity node)
         {
             GUILayout.BeginVertical();
@@ -347,13 +246,13 @@ namespace WorldTree
                 GUILayout.BeginHorizontal(GUIDefault.StyleBlack1);
                 GUILayout.BeginHorizontal(GUIDefault.StyleBlack2);
                 //GUILayout.Space(100);
-                GUILayout.Label("Components:", GUIDefault.StyleTransparent, GUIDefault.OptionWidth80);
+                GUILayout.Label("Components:", GUIDefault.StyleTransparent, GUIDefault.optionWidth80);
 
                 GUIDefault.LineVertical();
 
                 GUILayout.BeginVertical();
 
-
+            
                 foreach (var item in node.components)
                 {
                     TraversalRecursive(item.Value);
@@ -373,7 +272,7 @@ namespace WorldTree
                 GUILayout.BeginHorizontal(GUIDefault.StyleBlack1);
                 GUILayout.BeginHorizontal(GUIDefault.StyleBlack2);
 
-                GUILayout.Label("Children:", GUIDefault.StyleTransparent, GUIDefault.OptionWidth80);
+                GUILayout.Label("Children:", GUIDefault.StyleTransparent, GUIDefault.optionWidth80);
 
                 //GUILayout.Space(100);
 
@@ -381,7 +280,7 @@ namespace WorldTree
 
                 GUILayout.BeginVertical();
 
-
+        
 
                 foreach (var item in node.children)
                 {
@@ -476,4 +375,141 @@ namespace WorldTree
     }
 
 
+    #region ConsoleManager
+
+    public class ConsoleManager : Entity
+    {
+        public UnitDictionary<long, bool> ViewShows;
+        public SystemGroup systems;
+
+    }
+
+    class ConsoleManagerAddSystem : AddSystem<ConsoleManager>
+    {
+        public override void OnAdd(ConsoleManager self)
+        {
+            self.ViewShows = self.Root.ObjectPoolManager.Get<UnitDictionary<long, bool>>();
+            foreach (var item in self.Root.allEntities)
+            {
+                self.ViewShows.TryAdd(item.Key, true);
+            }
+        }
+    }
+
+    class ConsoleManagerRemoveSystem : RemoveSystem<ConsoleManager>
+    {
+        public override void OnRemove(ConsoleManager self)
+        {
+            self.ViewShows.Recycle();
+        }
+    }
+
+
+
+    class ConsoleManagerEntitySystem : EntitySystem<ConsoleManager>
+    {
+        public override void OnAddEntity(ConsoleManager self, Entity entity)
+        {
+            self.ViewShows.TryAdd(entity.id, false);
+        }
+
+        public override void OnRemoveEntity(ConsoleManager self, Entity entity)
+        {
+            self.ViewShows.Remove(entity.id);
+        }
+    }
+
+    //主控窗口Log消息：大小窗，FPS显示
+
+    //全局式树状图：多开，焦点
+
+    //跟踪挂载式信息面板
+
+
+    //窗口属于组件绘制父物体UI代码
+    //每个代码独立窗口
+    public class ConsoleWindow : Entity
+    {
+        public SystemGroup systems;
+
+        public Rect rect = new Rect(0, 0, 1000, 1000);
+        public void GUIWindowMax(int windowId)
+        {
+            GUI.DragWindow();
+
+            //子物体绘制
+        }
+    }
+
+    //class ConsoleWindowOnGUISystem : OnGUISystem<ConsoleWindow>
+    //{
+    //    public override void OnGUI(ConsoleWindow self, float deltaTime)
+    //    {
+    //        if (Event.current.type != EventType.Repaint)
+    //        {
+    //            self.rect = GUI.Window((int)self.id, self.rect, self.GUIWindowMax, "世界树控制台");
+    //        }
+
+    //    }
+
+    //}
+
+    class ConsoleWindowEvent : EventDelegate { }
+
+    public class WorldTreeConsole : Entity
+    {
+        public void GUITreeView()
+        {
+            GUILayout.BeginVertical();
+            GUILayout.Label(this.Parent.ToString());
+
+            GUILayout.BeginHorizontal();
+
+
+            GUILayout.Space(40);
+
+
+            GUILayout.BeginVertical();
+
+            UnitList<Type> TypeKeys = this.Root.ObjectPoolManager.Get<UnitList<Type>>();
+            foreach (var item in this.Parent.Components)
+            {
+                TypeKeys.Add(item.Key);
+            }
+
+            foreach (Type item in TypeKeys)
+            {
+                if (this.Parent.TryGetComponent(item, out Entity component))
+                {
+                    if (component.Type != this.Type)
+                    {
+                        component.AddComponent<WorldTreeConsole>().GUITreeView();
+                    }
+                }
+            }
+            TypeKeys.Recycle();
+
+            UnitList<long> Keys = this.Root.ObjectPoolManager.Get<UnitList<long>>();
+
+            foreach (var item in this.Parent.Children)
+            {
+                Keys.Add(item.Key);
+            }
+
+            foreach (long item in Keys)
+            {
+                if (this.Parent.TryGetChildren(item, out Entity entity))
+                {
+                    entity.AddComponent<WorldTreeConsole>().GUITreeView();
+                }
+            }
+            Keys.Recycle();
+            GUILayout.EndVertical();
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+        }
+    }
+    #endregion
 }
